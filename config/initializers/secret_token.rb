0099c1ad49bc774cda5bf73734bc6be4f1cea79d
Camron Weeks <camron.theindiebox.com>
2014-08-02 19:58:33 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'e22b27e36a07d540dfd31e219746fc689878a022ec5c30cd2520593f970f80b240c711e4d8f265a6b8f0b13e9b76ea098c5d5f8215ba8d0df57f75b7b513ef53'
